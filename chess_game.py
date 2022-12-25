e9cf557893c2d0f2db0dc27e09f73c47ffa5ebc8
Krupeshgithub <105969316+Krupeshgithub@users.noreply.github.com>
2022-12-25 07:49:47 +0000

    testing

--- chess_game.py
+++ chess_game.py
@@ -6,10 +6,10 @@ piece_dict = {"WHITE" : {"Pawn" : "♙", "Rook" : "♖", "Knight" : "♘", "Bish
 
 class Structure:
 
-    abc = ['S','a','b','c','d','e','f','g']
+    abc = ['S','a','b','c','d','e','f','g','h']
 
     def __init__ (self):
-        chess = np.zeros(shape=(8,8),dtype = 'object')
+        chess = np.zeros(shape=(9,8),dtype = 'str')
         for i in range(0,8):
             chess[0,i] = i
         count = 0
@@ -17,9 +17,15 @@ class Structure:
             chess[count,0] = i
             count+=1
         count = 1
-        for i in range(0,len(self.abc)):
-            chess[count,1] = piece_dict["WHITE"]["Pawn"]
+        temp = 1
+        for i in range(0,len(self.abc)-1):
+            chess[count,2] = piece_dict["WHITE"]["Pawn"]
+            if temp <= 5:
+                chess[count,1] = list(piece_dict["WHITE"].values())[temp]
+            chess[count,6] = piece_dict["BLACK"]["Pawn"]
             count+=1
+            temp+=1
+        count = 1
         print(chess)
 
 Structure()
